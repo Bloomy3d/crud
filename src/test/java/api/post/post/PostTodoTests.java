de3c01953a1548c27c81143f26c6cf73e58a39fc
Marat Galiev <m.galiev@xfer.world>
2025-01-22 19:58:34 +0400

    delete import

--- src/test/java/api/post/post/PostTodoTests.java
+++ src/test/java/api/post/post/PostTodoTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import specs.response.IncorrectDataResponse;
 
 class PostTodoTests extends BaseTest {
 
